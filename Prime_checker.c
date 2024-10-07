518e57ec5a36d6ce564680fe0a2ec38b1df7c173
Samipya Rijal <147675714+SamipyaRijal@users.noreply.github.com>
2024-10-06 20:17:16 -0600

    Add files via upload

---
+++ Prime_checker.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int prime(int number)
+{
+    
+    if (number == 1)
+        return 0;
+    
+    if (number == 2)
+            return 0;
+
+    
+    if (number%2 == 0)
+        return 0;
+
+
+    for (int i = 3; i < sqrt(number); i++)
+        if (number%i == 0)
+        return 0;
+
+
+    
+    return 1;
+
+}
+
+int main(void)
+{
+    int test_integer = 1;
+    
+
+    while (test_integer != 0){
+
+        printf("Enter an integer'(type 0 to end)':");
+            scanf("%d", &test_integer);
+     
+        if (prime(test_integer))
+            printf("Number is Prime\n");
+
+
+        else
+            printf("Number is not Prime\n");
+
+    }
+
+    return 0;
+}
